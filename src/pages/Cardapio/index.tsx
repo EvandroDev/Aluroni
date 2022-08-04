5c0e3477bf74a059a800afc3a33dd947ab3858ed
Evandro Martins <evandro.correa@gruposbf.com.br>
2022-08-04 11:41:29 -0300

    terminado curso

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,16 @@
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
+import Itens from './Itens';
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
+
   return (
     <main>
       <nav className={styles.menu}>
@@ -9,6 +18,15 @@ export default function Cardapio() {
         <header className={styles.header}>
           <div className={styles.header__text}>A casa blabla</div>
         </header>
+        <section className={styles.cardapio}>
+          <h3 className={styles.cardapio__titulo}>Cardapio</h3>
+          <Buscador busca={busca} setBusca={setBusca} />
+          <div className={styles.cardapio_filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro}/>
+            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+          </div>
+          <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>:
+        </section>
       </nav>
     </main>
   );
